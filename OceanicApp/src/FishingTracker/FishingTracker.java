87abfd3d0ef1cd319bd72b514f85abaa08110d02
Whitedyl <148541126+Whitedyl@users.noreply.github.com>
2024-11-17 22:44:27 +0000

    Add files via upload
    First Draft of Fishing Tracker App

---
+++ OceanicApp/src/FishingTracker/FishingTracker.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package FishingTracker;
+
+/**
+ *
+ * @author Dylan
+ */
+public class FishingTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number = 0;
+        FishermanGUI fishermanGUI = new FishermanGUI();
+        FishInputGUI fishInputGUI = new FishInputGUI();
+        
+        
+        
+        fishermanGUI.setVisible(true);
+    }
+    
+}
